86525005be660fcea5e9b5782724a3424aab5e69
Hans De Potter <hans_de_potter@hotmail.com>
2016-07-07 10:43:06 +0200

    Updated examples to latest version

--- examples/counter/src/counter/reducer.js
+++ examples/counter/src/counter/reducer.js
@@ -1,20 +1,18 @@
 
 export default (state = { counter: 0 }, action) => {
 
-	if (action.isLocal) {
-		switch (action.type) {
-			case 'INCREMENT_COUNTER': 
-				return {
-					...state,
-					counter: state.counter + 1
-				}
+	switch (action.type) {
+		case 'INCREMENT_COUNTER': 
+			return {
+				...state,
+				counter: state.counter + 1
+			}
 
-			case 'DECREMENT_COUNTER':
-				return {
-					...state,
-					counter: state.counter - 1
-				}
-		}
+		case 'DECREMENT_COUNTER':
+			return {
+				...state,
+				counter: state.counter - 1
+			}
 	}
 
 	return state
--- examples/list-of-counters/src/list-of-counters/reducer.js
+++ examples/list-of-counters/src/list-of-counters/reducer.js
@@ -1,20 +1,18 @@
 
 export default (state = { amount: 0 }, action) => {
 
-	if (action.isLocal) {
-		switch (action.type) {
-			case 'ADD_COUNTER': 
-				return {
-					...state,
-					amount: state.amount + 1
-				}
+	switch (action.type) {
+		case 'ADD_COUNTER': 
+			return {
+				...state,
+				amount: state.amount + 1
+			}
 
-			case 'REMOVE_COUNTER':
-				return {
-					...state,
-					amount: Math.max(0, state.amount - 1)
-				}
-		}
+		case 'REMOVE_COUNTER':
+			return {
+				...state,
+				amount: Math.max(0, state.amount - 1)
+			}
 	}
 
 	return state
